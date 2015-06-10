53db12a3f49742dbd198d0d7295fc3599f975c3d
Diogo Eiras <eiras@outlook.com>
2015-06-10 07:19:16 +0100

    Manual reference

--- src/tbda/Main.java
+++ src/tbda/Main.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 import tbda.model.Agenda;
 import tbda.model.Consulta;
@@ -15,6 +16,8 @@ import tbda.model.Medico;
 import com.google.gson.Gson;
 import com.mongodb.BasicDBObject;
 import com.mongodb.Block;
+import com.mongodb.DBCursor;
+import com.mongodb.DBRef;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.FindIterable;
@@ -87,7 +90,7 @@ public class Main {
 					"data/doente.json"));
 			Doente[] doenteData = gson.fromJson(br, Doente[].class);
 			for (Doente doenteValue : doenteData) {
-				doente.insertOne(new Document("codm", doenteValue.getCodd())
+				doente.insertOne(new Document("codd", doenteValue.getCodd())
 						.append("nome", doenteValue.getNome())
 						.append("NIF", doenteValue.getNIF())
 						.append("morada", doenteValue.getMorada())
@@ -110,9 +113,19 @@ public class Main {
 					"data/agenda.json"));
 			Agenda[] agendaData = gson.fromJson(br, Agenda[].class);
 			for (Agenda agendaValue : agendaData) {
+				
+				BasicDBObject query = new BasicDBObject("codm", agendaValue.getCodm());
+				Object id = null;
+				
+				for (Document doc : medico.find(query)) {
+					if((int)doc.get("codm") == agendaValue.getCodm()){
+						id = doc.get("_id");
+						break;
+					}
+				}
 				agenda.insertOne(new Document("nagenda", agendaValue
 						.getNagenda()).append("dia", agendaValue.getDia())
-						.append("codm", agendaValue.getCodm())
+						.append("codm", id)
 						.append("hora_inicio", agendaValue.getHora_inicio())
 						.append("no_doentes", agendaValue.getNo_doentes()));
 			}
@@ -130,12 +143,35 @@ public class Main {
 					"data/consulta.json"));
 			Consulta[] consultaData = gson.fromJson(br, Consulta[].class);
 			for (Consulta consultaValue : consultaData) {
+				Boolean found = false;
+				Object id = null ;
+				BasicDBObject query = new BasicDBObject("codm", consultaValue.getCodd());
+				
+				for (Document doc : medico.find(query)) {
+					if((int)doc.get("codm") == consultaValue.getCodd()){
+						found = true;
+						id = doc.get("_id");
+						break;
+					}
+				}
+				
+				if(!found){
+					query = new BasicDBObject("codd",consultaValue.getCodd());
+					for (Document doc: doente.find(query)){
+						if((int)doc.get("codd") == consultaValue.getCodd()){
+							found = true;
+							id = doc.get("_id");
+							break;
+						}
+					}
+				}
+				
 				consulta.insertOne(new Document("nagenda", consultaValue.getNagenda())
 						.append("hora", consultaValue.getHora())
 						.append("preço", consultaValue.getPreço())
 						.append("situação", consultaValue.getSituação())
 						.append("relatório", consultaValue.getRelatório())
-						.append("codd", consultaValue.getCodd()));
+						.append("codd", id));
 				Document modifier = new Document("no_doentes",1);
 				Document increment = new Document("$inc",modifier);
 				Document search = new Document("nagenda",consultaValue.getNagenda());
